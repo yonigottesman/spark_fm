b9c6cf9fbec4e68b12f2079c5d60319384eb47cb
Yonatan Gottesman <yonigo10@gmail.com>
2020-07-05 07:44:22 +0300

    index all movies all users not just after join

--- src/main/scala/TrainSparkFM.scala
+++ src/main/scala/TrainSparkFM.scala
@@ -33,41 +33,68 @@ object TrainSparkFM {
       .csv("ml-1m/movies.dat")
       .toDF("movie_id","title","genres")
 
+    val titleIndexer = new StringIndexer()
+      .setInputCol("title")
+      .setOutputCol("title_index")
+      .fit(movies)
+
+    val moviesIndexed = titleIndexer.transform(movies)
+
+    titleIndexer.write.overwrite().save("indexers/title")
+
     val users = spark.read.option("delimiter","::")
       .csv("ml-1m/users.dat")
       .toDF("user_id","gender","age","occupation","zipcode")
 
+
+    val userFeaturesIndexers = Array("user_id","gender","age","occupation")
+      .map(col => new StringIndexer().setInputCol(col).setOutputCol(col+"_index"))
+    val pipe = new Pipeline().setStages(userFeaturesIndexers).fit(users)
+
+    val usersIndexed = pipe.transform(users)
+    pipe.write.overwrite().save("indexers/user_features")
+
     val ratings = spark.read.option("delimiter","::")
-      .csv("fatoml-1m/ratings.dat")
+      .csv("ml-1m/ratings.dat")
       .toDF("user_id","movie_id","rating","time")
       .withColumn("rating",$"rating".cast(IntegerType))
-    
+
     val ratingsJoined = ratings
-      .join(users,Seq("user_id"))
-      .join(movies,Seq("movie_id"))
+      .join(usersIndexed,Seq("user_id"))
+      .join(moviesIndexed,Seq("movie_id"))
+
+    ratingsJoined.show(10)
 
 
+    // movies feature sizes
+    val numMovies = moviesIndexed
+      .select(countDistinct($"title_index").alias("numMovies"))
+      .take(1)(0).getAs[Long]("numMovies")
 
-    val indexers = Array("user_id","gender","age","occupation","title")
-      .map(col => new StringIndexer().setInputCol(col).setOutputCol(col+"_index"))
-    val pipe = new Pipeline().setStages(indexers)
-    val ratingsIndexed = pipe.fit(ratingsJoined).transform(ratingsJoined)
+    // users feature sizes
+    val userfeatureColumns = Seq("user_id_index","age_index","gender_index","occupation_index")
+    val userFeaturSizesDf = usersIndexed
+      .select(userfeatureColumns.map(c => countDistinct(col(c)).alias(c)): _*)
+    val userFeatureSizes = Map(userfeatureColumns
+      .zip(userFeaturSizesDf.take(1)(0).toSeq.map(_.asInstanceOf[Long])):_*)
 
+    //create offset map
     val featureColumns = Seq("user_id_index","title_index","age_index","gender_index","occupation_index")
-
-    val featureSizesDf = ratingsIndexed.select(featureColumns.map(c => countDistinct(col(c)).alias(c)): _*)
-    val featureSizes = Map(featureColumns.zip(featureSizesDf.take(1)(0).toSeq.map(_.asInstanceOf[Long])):_*)
+    val featureSizes = userFeatureSizes + ("title_index"->numMovies)
     val featureOffsets = Map(featureColumns.zip(featureColumns.scanLeft(0L)((agg,current)=>agg+featureSizes(current)).dropRight(1)):_*)
 
 
 
-    val ratings_input = ratingsIndexed
+    // add offset to each column
+    val ratingsInput = ratingsJoined
       .select(featureColumns.map(name=>(col(name) + lit(featureOffsets(name))).alias(name)):+$"rating":_*)
 
 
 
     val featureVectorSize = featureSizes.values.sum
-    val data = ratings_input
+
+    //convert rows to sparse vector
+    val data = ratingsInput
       .withColumn("features",createFeatureVectorUdf(lit(featureVectorSize)+:featureColumns.map(col):_*))
 
     val Array(trainingData, testData) = data.randomSplit(Array(0.9, 0.1))
@@ -77,29 +104,31 @@ object TrainSparkFM {
       .setLabelCol("rating")
       .setFeaturesCol("features")
       .setFactorSize(150)
-      .setStepSize(0.001)
+      .setStepSize(0.01)
 
+    trainingData.select("rating","features").show(10,false)
+    trainingData.printSchema()
     val model = fm.fit(trainingData)
 
     val predictions = model.transform(testData)
-
-    // Select example rows to display.
-    predictions.select("prediction", "rating", "features").show(5)
-
 
     val evaluator = new RegressionEvaluator()
       .setLabelCol("rating")
       .setPredictionCol("prediction")
       .setMetricName("rmse")
     val rmse = evaluator.evaluate(predictions)
+
     println(s"Root Mean Squared Error (RMSE) on test data = $rmse")
 
 
-    model.save("fmmodel")
+    model.write.overwrite().save("fmmodel")
 
     spark.close()
 
   }
+
+
+
 
   val createFeatureVectorUdf = udf((size:Int,
                                 user_id_index:Int,
